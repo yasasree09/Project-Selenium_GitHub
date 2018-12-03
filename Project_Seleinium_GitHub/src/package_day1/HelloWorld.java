6e2509db8baa29afbdb9669166bb6ff26f2a9a47
Sreeni Yasa <yasasree09@gmail.com>
2018-12-03 23:05:30 +0530

    day 1 commit

---
+++ Project_Seleinium_GitHub/src/package_day1/HelloWorld.java
@@ -0,0 +1,10 @@
+package package_day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!!");
+	}
+
+}
