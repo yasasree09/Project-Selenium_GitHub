91e3e3c90999205054f7065d82bec37aef0d3688
Sreeni Yasa <yasasree09@gmail.com>
2018-12-29 14:35:41 +0530

    changes till dec 29

--- Project_Seleinium_GitHub/src/casestudy6/CaseStudyTestAlreadyRegisteredEdurekaUser.java
+++ Project_Seleinium_GitHub/src/casestudy6/CaseStudyTestAlreadyRegisteredEdurekaUser.java
@@ -24,6 +24,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
@@ -109,6 +110,8 @@ public class CaseStudyTestAlreadyRegisteredEdurekaUser {
 
 		WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+		
+		Actions action = new Actions(driver);	
 
 	}
 
