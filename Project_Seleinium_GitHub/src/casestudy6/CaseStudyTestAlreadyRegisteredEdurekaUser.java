b26c9132803ddc85e73a5daf068dcbfa2f8adca4
Sreeni Yasa <yasasree09@gmail.com>
2018-12-29 14:34:16 +0530

    changes till dec 29

--- Project_Seleinium_GitHub/src/casestudy6/CaseStudyTestAlreadyRegisteredEdurekaUser.java
+++ Project_Seleinium_GitHub/src/casestudy6/CaseStudyTestAlreadyRegisteredEdurekaUser.java
@@ -5,7 +5,9 @@ Test the edureka portal to throw an exception while registering a user who alrea
 holds an account with the same email id. Log in to the edureka portal with the 
 registered email id,and search for all the courses available and Logout of the edureka 
 portal. Write testcases using TestNG for above scenario.
+
 Steps:
+
 1.Launch the Chrome browser
 2.Log in to the account and browser for all the courses
 3.Logout of the account
@@ -14,7 +16,6 @@ Steps:
 6.Check for the HTMLreport generated
 */
 
-
 package casestudy6;
 
 import java.util.concurrent.TimeUnit;
@@ -22,46 +23,17 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class CaseStudyTestAlreadyRegisteredEdurekaUser{
-	
+public class CaseStudyTestAlreadyRegisteredEdurekaUser {
+
 	WebDriver driver;
-	
-	@Test
-	public void testAlreadyRegisteredEdurekaUser() throws Exception{
-	
-		//Login into edureka site
-		driver.findElement(By.xpath("//a[text()='Sign Up']")).click();
-		waitFor(5000);
-		driver.findElement(By.xpath("//input[@id='sg_popup_email']")).sendKeys("yasasree09@gmail.com");
-		driver.findElement(By.xpath("//input[@id='sg_popup_phone_no']")).sendKeys("9890529927");
-		driver.findElement(By.xpath("//button[text()='Sign Up']")).click();
-		waitFor(8000);
-		
-		WebElement errorMessage = driver.findElement(By.xpath("//p[text()='You are registered with us. Login to continue.']"));
-		
-		if(errorMessage.isDisplayed()) {
-			System.out.println("Error thrown as user already exists!!");			
-		}		
-	}
-	
-	public void loginWithRegisteredUser(){
-		
-		//Login into edureka site
-				driver.findElement(By.xpath("//a[text()='Log In']")).click();
-				waitFor(5000);
-				driver.findElement(By.xpath("//input[@id='si_popup_email']")).sendKeys("sreeniyasa@gmail.com");
-				driver.findElement(By.xpath("//input[@id='si_popup_passwd']")).sendKeys("**********");
-				driver.findElement(By.xpath("//button[text()='Login']")).click();
-				waitFor(8000);
-	}
-	
+
 	@BeforeClass
 	private void launchBrowser() {
 
@@ -74,15 +46,70 @@ public class CaseStudyTestAlreadyRegisteredEdurekaUser{
 		driver.manage().timeouts().pageLoadTimeout(1200, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
-	
-	private void waitFor(long waitTime) {
 
+	@Test(priority = 0)
+	public void testAlreadyRegisteredEdurekaUser() throws Exception {
+
+		// Login into edureka site
+		driver.findElement(By.xpath("//a[text()='Sign Up']")).click();
+		waitFor(5000);
+		driver.findElement(By.xpath("//input[@id='sg_popup_email']")).sendKeys("yasasree09@gmail.com");
+		driver.findElement(By.xpath("//input[@id='sg_popup_phone_no']")).sendKeys("9890529927");
+		driver.findElement(By.xpath("//button[text()='Sign Up']")).click();
+		waitFor(8000);
+
+		WebElement errorMessage = driver
+				.findElement(By.xpath("//p[text()='You are registered with us. Login to continue.']"));
+
+		if (errorMessage.isDisplayed()) {
+			System.out.println("Error thrown as user already exists!!");
+		}
+	}
+
+	@Test(priority = 1)
+	public void logInUser() {
+
+		// Login into edureka site with existing user
+		driver.findElement(By.xpath("//p[contains(text(),'Already have an account?')]//span[text()='Log In']")).click();
+		waitFor(5000);
+		driver.findElement(By.xpath("//input[@id='si_popup_email']")).sendKeys("yasasree09@gmail.com");
+		driver.findElement(By.xpath("//input[@id='si_popup_passwd']")).sendKeys("**********");
+		driver.findElement(By.xpath("//button[text()='Login']")).click();
+		waitFor(8000);
+	}
+
+	@Test(priority = 2)
+	public void searchAllCourses() {
+		// click on All Courses
+		driver.findElement(By.xpath("//a[@id='dLabel'][@class='trackHover trackButton']")).click();
+
+		// wait till element is visible
+		waitTillElementVisible(10, By.xpath("//h3[contains(text(),'AWS Architect Certification Training')]"));
+		waitFor(8000);
+	}
+
+	@Test(priority = 3)
+	public void logOut() {
+		// Logout from edureka site
+		driver.findElement(By.xpath("//a[@class='dropdown-toggle trackButton']//img[@class='img30']")).click();
+		driver.findElement(By.xpath("//li[@class='dropdown userdrop hidden-sm hidden-xs open']//a[text()='Log Out']"))
+				.click();
+		waitFor(8000);
+	}
+
+	private void waitFor(long waitTime) {
 		try {
 			Thread.sleep(waitTime);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	private void waitTillElementVisible(int timeOutInSeconds, By by) {
+
+		WebDriverWait wait = new WebDriverWait(driver, timeOutInSeconds);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(by));
+
 	}
 
 	@AfterClass
