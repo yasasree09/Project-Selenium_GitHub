26b30d9518a4a19a8cbb433262297bc03a72826e
Sreeni Yasa <yasasree09@gmail.com>
2019-01-03 10:23:50 +0530

    changes of case study8

--- Project_Seleinium_GitHub/src/Casestudy8_without_pagefactory/GmailHomePage.java
+++ Project_Seleinium_GitHub/src/Casestudy8_without_pagefactory/GmailHomePage.java
@@ -6,23 +6,24 @@ import org.openqa.selenium.WebDriver;
 public class GmailHomePage {
 
 	WebDriver driver;
-	
+
 	private By compose = By.className("T-I J-J5-Ji T-I-KE L3");
 	private By googleAccount = By.className("gb_cb gbii");
 	private By signOut = By.xpath("//a[text()='Sign out']");
-	
-	public GmailHomePage(WebDriver driver) {		
-		this.driver=driver;		
+
+	public GmailHomePage(WebDriver driver) {
+		this.driver = driver;
 	}
-	public void googleAccount() {		
-		driver.findElement(googleAccount).click();		
-	}	
-	
-	public void signOut() {		
-		driver.findElement(signOut).click();		
+
+	public void googleAccount() {
+		driver.findElement(googleAccount).click();
 	}
-	
-	public void compose() {		
-		driver.findElement(compose).click();		
+
+	public void signOut() {
+		driver.findElement(signOut).click();
+	}
+
+	public void compose() {
+		driver.findElement(compose).click();
 	}
 }
